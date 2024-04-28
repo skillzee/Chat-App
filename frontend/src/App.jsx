a2b28addb15c5916954f0169491d4d2e4d36fc30
Parth Sharma <112481963+skillzee@users.noreply.github.com>
2024-04-28 12:36:06 +0530

    Complete Ui Finished

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,16 @@
 
 import './App.css'
+import Home from './pages/home/Home'
 import Login from './pages/login/Login'
-import SignUp from './pages/signup/Signup'
+// import SignUp from './pages/signup/Signup'
 
 function App() {
 
 
   return (
    <div className=' p-4 h-screen flex items-center justify-center'>
-    <SignUp/>
+    {/* <SignUp/> */}
+    <Home/>
    </div>
 
   )
